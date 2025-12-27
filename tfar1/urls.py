3543dfeee5eca6cde82b5de1a3a4fd68362204e2
cat-baloo <alex@cat-baloo.com>
2025-12-28 05:17:17 +1100

    Update urls.py

--- tfar1/urls.py
+++ tfar1/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("upload/", views.upload_tfar, name="upload_tfar"),
     path("download/", views.download_tfar_csv, name="download_tfar_csv"),
     #path("debug/", views.debug_view),
-    #path("check/", views.safe_debug),
+    path("check/", views.safe_debug),
 ]
