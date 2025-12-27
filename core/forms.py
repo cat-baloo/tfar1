e3f7601263715c2d40f34c86488b2bac623788c0
cat-baloo <alex@cat-baloo.com>
2025-12-28 03:46:56 +1100

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,32 @@
 
 from django import forms
+from .models import ClientMembership, Client
+
 class UploadForm(forms.Form):
-    file = forms.FileField(help_text="Upload .xlsx with 15 columns")
+    file = forms.FileField(help_text="Upload .xlsx with 15 headers")
+    client = forms.ChoiceField(choices=[], label="Client")  # dynamic
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        self.fields["client"].choices = self._client_choices(user)
+
+    def _client_choices(self, user):
+        if user is None or not user.is_authenticated:
+            return []
+        memberships = ClientMembership.objects.filter(user=user).select_related("client").order_by("client__name")
+        return [(str(m.client.id), m.client.name) for m in memberships]
+
+class ClientSelectForm(forms.Form):
+    client = forms.ChoiceField(choices=[], label="Client")
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        self.fields["client"].choices = self._client_choices(user)
+
+    def _client_choices(self, user):
+        if user is None or not user.is_authenticated:
+            return []
+        memberships = ClientMembership.objects.filter(user=user).select_related("client").order_by("client__name")
+        return [(str(m.client.id), m.client.name) for m in memberships]
