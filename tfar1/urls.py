a33dbb8b56bb021fb7677428523a7892d9db83b1
cat-baloo <alex@cat-baloo.com>
2025-12-28 01:35:39 +1100

    Create urls.py

---
+++ tfar1/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("", views.dashboard, name="dashboard"),
+    path("upload/", views.upload_tfar, name="upload_tfar"),
+    path("download/", views.download_tfar_csv, name="download_tfar_csv"),
+]
